710d971de3eacfec294f4a567efc711dfeb13e2f
zedsalim <salimzaiidi@gmail.com>
2024-06-18 21:19:55 +0100

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // Opacity for the navbar
-window.addEventListener("scroll", function () {
-  var navbar = document.querySelector(".navbar");
-  if (window.scrollY > 0) {
-    navbar.style.opacity = "0.9";
-  } else {
-    navbar.style.opacity = "1";
-  }
-});
+// window.addEventListener("scroll", function () {
+//   var navbar = document.querySelector(".navbar");
+//   if (window.scrollY > 0) {
+//     navbar.style.opacity = "0.95";
+//   } else {
+//     navbar.style.opacity = "1";
+//   }
+// });
