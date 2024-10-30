0a5ed8a21fe9511b265b89f37fdf3a3e6f3b1032
Samy Bacha <sami.bacha@univ-bouira.dz>
2024-10-30 18:23:57 +0100

    Language Switcher Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,21 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+/* ---- Language switcher ---- 
+function switchLanguage(lang) {
+  const script = document.createElement('script');
+  script.src = `./js/lang-${lang}.js`;
+  document.head.appendChild(script);
+  script.addEventListener('load', () => {
+    document.addEventListener('DOMContentLoaded', () => {
+      document.getElementById('welcomeMessage').textContent = translations.welcomeMessage;
+      document.getElementById('description').textContent = translations.description;
+      document.getElementById('grades').textContent = translations.grades;
+      document.getElementById('licence1').textContent = translations.licence1;
+      document.getElementById('licence2').textContent = translations.licence2;
+    });
+  });
+
+  document.head.appendChild(script);
+}
+*/
