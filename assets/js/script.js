d4f9e799f07d6837553fcb072e5e83eb0d373e5d
zedsalim <salimzaiidi@gmail.com>
2024-11-01 13:47:53 +0100

    Remove unused JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,38 +17,3 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-/* ---- Language switcher ---- 
-function switchLanguage(lang) {
-  const script = document.createElement('script');
-  script.src = `./js/lang-${lang}.js`;
-  document.head.appendChild(script);
-  script.addEventListener('load', () => {
-    document.addEventListener('DOMContentLoaded', () => {
-      document.getElementById('welcomeMessage').textContent = translations.welcomeMessage;
-      document.getElementById('description').textContent = translations.description;
-      document.getElementById('grades').textContent = translations.grades;
-      document.getElementById('licence1').textContent = translations.licence1;
-      document.getElementById('licence2').textContent = translations.licence2;
-    });
-  });
-
-  document.head.appendChild(script);
-}
-
-/*---- Dark mode toggle ---- 
-
-const darkModeToggle = document.getElementById('darkModeToggle');
-const header = document.querySelector('.navbar');
-
-darkModeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-  header.classList.toggle('dark-mode');
-
-  if (document.body.classList.contains('dark-mode')) {
-    darkModeToggle.textContent = 'Light Mode';
-  } else {
-    darkModeToggle.textContent = 'Dark Mode';   
-
-  }
-});
-*/
